c405960f2672280aab91ad1a5767a00dca712dfe
B. Haki Martin <jojackblack@gmail.com>
2024-03-28 11:45:59 -0400

    set up login form as a reactive form. Imported ReactiveFormsModule, FormControl and FormGroup into login-page.ts file and created userInfo:FormGroup property and onSubmitForm() property

--- Multi-MediaApp/src/app/app.component.ts
+++ Multi-MediaApp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- Multi-MediaApp/src/app/login-page/login-page.component.ts
+++ Multi-MediaApp/src/app/login-page/login-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.sass'
 })
 export class LoginPageComponent {
+  userInfo:FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
 
+  onSubmitForm(){
+    console.log(this.userInfo);
+  }
 }
