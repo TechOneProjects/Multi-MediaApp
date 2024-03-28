65e41a7fd849e0adbedab99a69ebd25beb3e1258
Janette Campbell <campbjp@hotmail.com>
2024-03-28 11:16:38 -0400

    chore: generated component for about us page

---
+++ Multi-MediaApp/src/app/general-about-us-page/general-about-us-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-about-us-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './general-about-us-page.component.html',
+  styleUrl: './general-about-us-page.component.sass'
+})
+export class GeneralAboutUsPageComponent {
+
+}
