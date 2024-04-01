92ce58fb20278e26d9362715a0a394fb06d9b300
B. Haki Martin <jojackblack@gmail.com>
2024-03-31 22:32:47 -0400

    created galleryPageComponent, and injected it with httpClient and imported infiniteScrollModule

---
+++ Multi-MediaApp/src/app/gallery-page/gallery-page.component.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+@Component({
+  selector: 'app-gallery-page',
+  standalone: true,
+  imports: [InfiniteScrollModule],
+  templateUrl: './gallery-page.component.html',
+  styleUrl: './gallery-page.component.sass'
+})
+export class GalleryPageComponent {
+  http = inject(HttpClient);
+}
