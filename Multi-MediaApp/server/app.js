78bd7b1b12125d6ee3553f23990dd100ce205caa
B. Haki Martin <jojackblack@gmail.com>
2024-04-04 11:49:44 -0400

    added album route

--- Multi-MediaApp/server/app.js
+++ Multi-MediaApp/server/app.js
@@ -4,12 +4,17 @@ const cors = require("cors");
 
 const app = express();
 const MONGODB_URI = "mongodb+srv://Admin:TechOne2401@multi-media-app.nywmu3r.mongodb.net/?retryWrites=true&w=majority&appName=Multi-Media-App"
+
+// controller imports
 const auth = require("./controllers/authController.js");
+const albums = require("./controllers/AlbumController.js");
 
 app.use(express.json());
 app.use(cors());
 
+// controllers and their routes
 app.use('/auth', auth);
+app.use('/albums', auth);
 
 app.get("/", (req, res) => {
     res.send({message: "Hello"});
@@ -18,9 +23,3 @@ app.get("/", (req, res) => {
 mongoose.connect(MONGODB_URI).then(() => {
     app.listen(3000);
 })
-
-/*
-mongodb+srv://Admin:TechOne2401@multi-media-app.nywmu3r.mongodb.net/?retryWrites=true&w=majority&appName=Multi-Media-App
-mongodb+srv://Admin:TechOne2401@multi-media-app.nywmu3r.mongodb.net/?retryWrites=true&w=majority&appName=Multi-Media-App
-mongodb+srv://Admin:TechOne2401@multi-media-app.nywmu3r.mongodb.net/Multi-Media-App?retryWrites=true&w=majority&appName=Multi-Media-App
-*/
