c7d916f898dcd47f95c582ee18777c8f72d7be58
Janette Campbell <campbjp@hotmail.com>
2024-04-04 19:47:40 -0400

    chore: add routerLink to page and verify user to view page.

--- Multi-MediaApp/src/app/general-about-us-page/general-about-us-page.component.ts
+++ Multi-MediaApp/src/app/general-about-us-page/general-about-us-page.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-general-about-us-page',
   standalone: true,
-  imports: [MatTabsModule, MatButtonModule],
+  imports: [MatTabsModule, MatButtonModule, RouterLink, RouterLinkActive],
   templateUrl: './general-about-us-page.component.html',
   styleUrl: './general-about-us-page.component.sass',
 })
