aa2968b04b0f8f3515c8f9301493841a679888b7
Janette Campbell <campbjp@hotmail.com>
2024-04-04 15:33:46 -0400

    chore: add background image

--- Multi-MediaApp/src/app/app.routes.ts
+++ Multi-MediaApp/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { JanetteComponent } from './janette/janette.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "janette", component:JanetteComponent }
+];
---
+++ Multi-MediaApp/src/app/janette/janette.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-janette',
+  standalone: true,
+  imports: [],
+  templateUrl: './janette.component.html',
+  styleUrl: './janette.component.sass'
+})
+export class JanetteComponent {
+
+}
