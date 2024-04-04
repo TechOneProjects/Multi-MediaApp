f0fe8c11974ca6f43257c1137a93f9e072ad7db6
Janette Campbell <campbjp@hotmail.com>
2024-04-04 18:53:16 -0400

    chore: increase height of about me box add about me button

--- Multi-MediaApp/src/app/app.routes.ts
+++ Multi-MediaApp/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { GeneralAboutUsPageComponent } from './general-about-us-page/general-about-us-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "about", component: GeneralAboutUsPageComponent}
+];
