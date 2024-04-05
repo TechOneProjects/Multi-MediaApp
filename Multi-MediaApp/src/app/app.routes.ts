9d9b2bfb1cace28f470307853b8fb0348f6b2a68
Janette Campbell <campbjp@hotmail.com>
2024-04-05 08:44:41 -0400

    changed redirect to root route.

--- Multi-MediaApp/src/app/app.routes.ts
+++ Multi-MediaApp/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { JanetteComponent } from './janette/janette.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   {path: "janette", component: JanetteComponent }
