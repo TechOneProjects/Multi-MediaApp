c2036297923664f86b6c90d4c422797cb4fccb4a
B. Haki Martin <jojackblack@gmail.com>
2024-04-09 01:22:19 -0400

    created service for gallery. Moved functions over to gallery service.

---
+++ Multi-MediaApp/src/app/services/gallery-helper.service.ts
@@ -0,0 +1,88 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GalleryHelperService implements OnInit {
+  constructor() { }
+
+  private http:HttpClient = inject(HttpClient);
+  private serverAddress = 'http://localhost:3000/gallery';
+  public gallery:{
+    title:string,
+    imageURL:string,
+    altText:string,
+    uid:String
+    _id:String
+  }[] = [];
+
+  ngOnInit(): void {
+    this.http.get(`${this.serverAddress}/`).subscribe( (res)=>{
+      this.gallery = res as [];
+    })
+  }
+
+  addImageToGallery(data:{title:string, imageURL:string, altText:string}){
+    const imageObj:{ title:string, imageURL:string, altText:string, uid:String } = {
+      title: data.title,
+      imageURL: data.imageURL,
+      altText: data.altText,
+      uid: ""
+    }
+    this.http.post(`${this.serverAddress}/`, imageObj).subscribe(res => {
+      console.log(res);
+      this.gallery.push(res as {
+        _id: string;
+        uid: string;
+        title: string;
+        imageURL: string;
+        altText: string;
+      })
+    })
+  }
+
+  deleteImage(id:String):void{
+    this.http.delete(`${this.serverAddress}/${id}`).subscribe( res =>{
+      this.gallery = this.gallery.filter( (image:{_id:String}) => {
+        if(image._id != id){
+          return image;
+        }
+        else return null;
+      })
+    })
+  }
+
+  updateImage(imageObj:{
+    title:string,
+    imageURL:string,
+    altText:string,
+    _id:string
+  }){
+    const data:{
+      title:string,
+      imageURL:string,
+      altText:string
+    } = {
+      title:imageObj.title,
+      imageURL:imageObj.imageURL,
+      altText:imageObj.altText
+    }
+
+    this.http.put(`${this.serverAddress}/${imageObj._id}`, data).subscribe( (res) => {
+      this.gallery = this.gallery.map( (image) => {
+        if(image._id === imageObj._id){
+          image = res as {
+            title:string,
+            imageURL:string,
+            altText:string,
+            _id:string,
+            uid:string
+          };
+        }
+        return image;
+      })
+    })
+  }
+
+}
