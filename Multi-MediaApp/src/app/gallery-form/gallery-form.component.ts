784bd760c443c2830b8653ae36ecb1b39b06cd9f
B. Haki Martin <jojackblack@gmail.com>
2024-04-09 22:43:00 -0400

    galleryForm component now uses Angular materials

--- Multi-MediaApp/src/app/gallery-form/gallery-form.component.ts
+++ Multi-MediaApp/src/app/gallery-form/gallery-form.component.ts
@@ -1,11 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GalleryHelperService } from '../services/gallery-helper.service';
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-gallery-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './gallery-form.component.html',
   styleUrl: './gallery-form.component.sass'
 })
