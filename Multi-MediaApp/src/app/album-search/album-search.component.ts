bd3062281b99d293288ea02acb2539ad685217e9
Chase Forlini <cforlini24@gmail.com>
2024-04-12 17:15:18 -0400

    removed unneeded imports

--- Multi-MediaApp/src/app/album-search/album-search.component.ts
+++ Multi-MediaApp/src/app/album-search/album-search.component.ts
@@ -6,7 +6,6 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-import jwt, { JwtPayload } from 'jsonwebtoken'
 
 @Component({
   selector: 'app-album-search',
