75816a1f3e01b1ccea6826d20272809805096b32
B. Haki Martin <jojackblack@gmail.com>
2024-04-11 23:12:35 -0400

    added profile, updated section in about-us page, updated about-me

--- Multi-MediaApp/src/app/app.component.ts
+++ Multi-MediaApp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GeneralAboutUsPageComponent } from './general-about-us-page/general-about-us-page.component';
+import { AboutHakiComponent } from './about-haki/about-haki.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GeneralAboutUsPageComponent],
+  imports: [RouterOutlet, GeneralAboutUsPageComponent, AboutHakiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
