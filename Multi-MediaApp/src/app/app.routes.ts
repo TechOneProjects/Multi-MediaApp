e65c3c45621012a2d24e16961fe980ed3a5a70d1
badihaki <jojackblack@gmail.com>
2024-04-12 21:20:40 -0400

    finished peer review for gallery

--- Multi-MediaApp/src/app/app.routes.ts
+++ Multi-MediaApp/src/app/app.routes.ts
@@ -4,17 +4,23 @@ import { AppComponent } from './app.component';
 import { MediaPage2Component } from './media-page-2/media-page-2.component';
 import { ChasesMusicComponent } from './chases-music/chases-music.component';
 import { AboutChaseComponent } from './about-chase/about-chase.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
 
 export const routes: Routes = [
     {
         path: "music", component: ChasesMusicComponent, title: "Music"
     },
 
-  {path: "janette", component: JanetteComponent },
-  {path: "movie", component: MediaPage2Component},
+    { path: "janette", component: JanetteComponent },
+    { path: "movie", component: MediaPage2Component },
     {
         path: "chase",
         component: AboutChaseComponent,
         title: "Chase"
+    },
+    {
+        path: "gallery",
+        component: GalleryPageComponent,
+        title: "MultiMediaApp | Gallery"
     }
 ];
