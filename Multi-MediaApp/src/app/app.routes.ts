05a1afeecf2f3495181a63c2804e4bb0b4dd44a6
badihaki <jojackblack@gmail.com>
2024-04-12 20:58:10 -0400

    finished peer review for about me page

--- Multi-MediaApp/src/app/app.routes.ts
+++ Multi-MediaApp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutHakiComponent } from './about-haki/about-haki.component';
+import { GeneralAboutUsPageComponent } from './general-about-us-page/general-about-us-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: GeneralAboutUsPageComponent
+    },
+    {
+        path: "haki",
+        component: AboutHakiComponent
+    }
+];
