9cee38adf9a755df0b6bb96c3ece71eadfeff77a
Daniel <davalencia66@gmail.com>
2024-04-14 04:01:27 -0400

    added my about me page

--- Multi-MediaApp/src/app/app.routes.ts
+++ Multi-MediaApp/src/app/app.routes.ts
@@ -3,13 +3,19 @@ import { JanetteComponent } from './janette/janette.component';
 import { AppComponent } from './app.component';
 import { MediaPage2Component } from './media-page-2/media-page-2.component';
 import { AboutChaseComponent } from './about-chase/about-chase.component';
+import { AboutDanielVComponent } from './about-daniel-v/about-daniel-v.component';
+import path from 'path';
 
 export const routes: Routes = [
-  {path: "janette", component: JanetteComponent },
-  {path: "movie", component: MediaPage2Component},
-    {
-        path: "chase",
-        component: AboutChaseComponent,
-        title: "Chase"
-    }
+  { path: "janette", component: JanetteComponent },
+  { path: "movie", component: MediaPage2Component },
+  {
+    path: "chase",
+    component: AboutChaseComponent,
+    title: "Chase"
+  },
+  {
+    path: "daniel-v", component: AboutDanielVComponent,
+    title: "Daniel-V"
+  },
 ];
