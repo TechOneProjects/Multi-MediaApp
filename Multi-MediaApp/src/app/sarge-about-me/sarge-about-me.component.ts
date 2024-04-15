a158a3509ee801a14124ec5aa6877159782705fe
Daniel-Ross Sargeant <DKSargeant@fullsail.edu>
2024-04-14 21:34:10 -0400

    about me really added

--- Multi-MediaApp/src/app/sarge-about-me/sarge-about-me.component.ts
+++ Multi-MediaApp/src/app/sarge-about-me/sarge-about-me.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { AboutMeVideosAboutMeSongComponent } from '../about-me-videos-about-me-song/about-me-videos-about-me-song.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-sarge-about-me',
   standalone: true,
-  imports: [AboutMeVideosAboutMeSongComponent ,RouterOutlet,RouterLink],
+  imports: [AboutMeVideosAboutMeSongComponent ,RouterOutlet,RouterLink,MatDividerModule, MatCardModule],
   templateUrl: './sarge-about-me.component.html',
   styleUrl: './sarge-about-me.component.sass'
 })
